a5d338f7e25b8f175b1ebc8aaf0d50dc54f83d84
bluepill5 <alexandro.mayoral@gmail.com>
2022-03-19 13:53:43 -0600

    Readme fix

--- work_04/hbs/src/server.js
+++ work_04/hbs/src/server.js
@@ -1,8 +1,55 @@
+/* -------------------------------- Librerias ------------------------------- */
 const express = require('express');
+const Contenedor = require('../../Contenedor');
 
+
+/* -------------------------------- Productos ------------------------------- */
+let path_file = './work_03/productos.json';
+async function get_products(path_file) {
+    const container = new Contenedor(path_file);
+    let prods = await container.getAll();
+    return prods;
+}
+
+async function get_product(path_file, id) {
+    const container = new Contenedor(path_file);
+    let prod = await container.getById(id);
+    return prod;
+}
+
+async function post_product(path_file, newProduct) {
+    const container = new Contenedor(path_file);
+    let new_prod_id = await container.save(newProduct);
+    return new_prod_id;
+}
+
+async function update_product(path_file, id, prodUpdate) {
+    const container = new Contenedor(path_file);
+    let prod = await container.updateById(id, prodUpdate);
+    return prod;
+}
+
+async function delete_product(path_file, id) {
+    const container = new Contenedor(path_file);
+    let prod = await container.deleteById(id);
+    return prod;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                              Servidor Express                              */
+/* -------------------------------------------------------------------------- */
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.set('views', './src/views');
+app.set('view engine', 'hbs');
+
+/* -------------------------------- Endpoints ------------------------------- */
+// Home
+app.get('', (req, res) => {
+    res.sendFile('index.html');
+});
 
 const PORT = 8080;
 const server = app.listen(PORT, () => {
