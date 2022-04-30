7386f9f924834a3c443930861189f0485ac8e13c
bluepill5 <alexandro.mayoral@gmail.com>
2022-04-30 07:50:27 -0500

    Normalizr

---
+++ scripts/normalization/norm1.js
@@ -0,0 +1,47 @@
+const { normalize, schema } = require('normalizr');
+
+let blogposts = {
+  id: 10,
+  title: "My blogpost",
+  description: "Short blogpost description",
+  content: "Hello world",
+  author: { id: 1, name: "John Doe" },
+  comments: [
+    {
+      id: 1,
+      author: { id: 2, name: "Rob" },
+      content: "Nice post!",
+    },
+    {
+      id: 2,
+      author: { id: 3, name: "Jane" },
+      content: "I totally agree with you",
+    },
+  ],
+};
+
+const authSchema = new schema.Entity('authors');
+const commentSchema = new schema.Entity('comments', {
+    author: authSchema
+});
+
+const postSchema = new schema.Entity('post', {
+    author: authSchema,
+    comments: [commentSchema]
+});
+
+const dataNormalized = normalize(blogposts, postSchema);
+
+// console.log(dataNormalized);
+
+const util = require('util');
+
+function print(data) {
+    console.log(util.inspect(data, false, 12, true));
+}
+
+print(dataNormalized);
+
+console.log(JSON.stringify(blogposts).length, JSON.stringify(dataNormalized).length);
+
+
