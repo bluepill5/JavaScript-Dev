7e433bf6ddc0875bdf4fec13587072306f32502a
bluepill5 <alexandro.mayoral@gmail.com>
2022-05-07 15:08:22 -0500

    connect-mongo

--- scripts/session/src/server.js
+++ scripts/session/src/server.js
@@ -1,29 +1,49 @@
 import express, { json, urlencoded } from 'express';
 import dotenv from 'dotenv';
 import session from 'express-session';
+import cookieParser from 'cookie-parser';
+import mongoStore from 'connect-mongo';
 // import FileStore from 'session-file-store';
-import redis from 'redis';
-import RedisStore from 'connect-redis';
-import auth from './middleware/auth.middleware.js'; 
-
+// import redis from 'redis';
+// import RedisStore from 'connect-redis';
+import auth from './middleware/auth.middleware.js';  
 dotenv.config();
 
 // const fileStore = FileStore(session);
-const client = redis.createClient(
-    process.env.PORT_REDIS,
-    process.env.HOST_REDIS
-);
+// const client = redis.createClient(
+//     process.env.PORT_REDIS,
+//     process.env.HOST_REDIS
+// );
+// const client = redis.createClient({ legacyMode: true });
+// client.connect();
+
+// const redisStore = RedisStore(session);
 
 const app = express();
+app.use(cookieParser());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
 app.use(session({
-    store: new fileStore({
-        path: './sessions',
-        ttl: 300,
-        retries: 0,
+    // store: new fileStore({
+    //     path: './sessions',
+    //     ttl: 300,
+    //     retries: 0,
+    // }),
+    // store: new redisStore({
+    //     host: 'localhost',
+    //     port: 6379,
+    //     client,
+    //     ttl: 300
+    // }),
+    store: mongoStore.create({
+        mongoUrl: process.env.MONGO_URI,
+        options: {
+            userNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
     }),
+
     secret: process.env.SECRET,
     resave: true,
     saveUninitialized: true, 
